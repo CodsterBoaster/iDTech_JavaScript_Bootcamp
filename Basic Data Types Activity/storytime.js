e3dd13fd3f1f20af1fb6bff89e32aca0f969110a
CodsterBoaster <magmaknightcody@gmail.com>
2022-01-29 10:52:44 -0600

    new file

---
+++ Basic Data Types Activity/storytime.js
@@ -0,0 +1,22 @@
+// TODO: Create 4-String variables to introduce develop your story.
+let greeting = "Hi";
+let name = "Jeb";
+let title = "king";
+let worldName = "lol"; 
+
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+let age = "100";
+let year = "2000";
+let century = "20";
+
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+
+let usedWeapon = ["sword","stick", "bow"];
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+let favWeapon = true;
+
+// TODO: Print your story to the console.
+console.log(greeting + ", my name is " + name + ", and I am the " + title + ". the world is called " + worldName + ".");
+console.log("My age is " + age + ". And the year here is " + year + ", and it is the " +century+ "th century.");
+console.log("And it is " + favWeapon + ", I have a favorite weapon. And it is the " + usedWeapon[0] + ".");
